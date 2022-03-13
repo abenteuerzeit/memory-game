49b06bfedf2d5dd392d395aace81ed3c82b6aae2
abenteuerzeit <amroz.muzyka@gmail.com>
2022-03-13 14:08:13 +0100

    draw board

--- memory_game.py
+++ memory_game.py
@@ -1,5 +1,6 @@
 import os
 import random
+from re import I
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -22,14 +23,18 @@ def generate_board(height, width):
         print("Error! \nEither the height or width is an odd number,\nor not enough letters in the latin alphabet to generate the board.")
 
 
-# Get field position from User
-
-# The user can select a field on the board, by typing in its coordinates, such as B2. The letter denotes the column (A is 0, B is 1, and so on...) and the number denotes the row. Do not forget that 1 actually refers to 0 index.
+def get_user_field_position():
+    board_fields = {}
+    
+# The user can select a field on the board, by typing in its coordinates, such as B2. 
+# The letter denotes the column (A is 0, B is 1, and so on...) and the number denotes the row. Do not forget that 1 actually refers to 0 index.
 # The program checks whether the user input is in the LETTERNUMBER format, such as B2 or C4.
 # The program checks whether the input number is positive and does not exceed the number of rows on the board.
 # The program checks whether the position of the input letter in the alphabet does not exceed the number of columns on the board.
 # The program keeps asking the user for a field until receiving valid input.
 # After the input is validated, coordinates of the selected field are passed further into the program logic as an integer tuple (row, column).
+    pass 
+
 def display_menu():
     print("Select a difficulty level. Enter the corresponding number. ")
     print("1. Easy")
@@ -58,28 +63,27 @@ def get_difficulty():
             print("\nInvalid input. Try again. ")
 
 
+def draw_board(board):
+    width = len(board)
+    columns = [letter for letter in alphabet[:width-1]]
+    print("\n     " + " ".join(columns) + "\n")
+    for count, row in enumerate(board):
+        print(f"{count + 1}    " + " ".join(row))
+    print("")
 
-# Implement the board drawing mechanic, according to the following requirements.
-# When drawing the board, the coordinates of fields are also be displayed (numbers of rows and letters of columns), similar to the following example:
-#   A B C D E
-
-# 1 # # # # #
-
-# 2 # # # # #
-
-# 3 # # # # #
-
-# 4 # # # # #
-# During the game, the fields can be either concealed or revealed. The letter of the field is displayed only when the field is revealed. When the field is concealed, # is displayed.
+# During the game, the fields can be either concealed or revealed. 
+# The letter of the field is displayed only when the field is revealed. 
+# When the field is concealed, # is displayed.
 # The terminal window is cleaned every time the board is redrawn.
 
+def main():
+    print("Welcome to Memory Game!")
+    board_size = get_difficulty()
+    console_clear()
+    board = generate_board(height=board_size[0], width=board_size[1])
+    draw_board(board)
 
-# Game logic
 
-# Implement the game logic, to put all parts together into a functioning game.
-# The game opens with Welcome to Memory Game!.
-# The game asks to select a difficulty level.
-# The letter board is generated, based on the selected difficulty level.
 # The fields of the letter board are all concealed by default.
 # The board is displayed to the user.
 # The game asks asked to select a field, which is then revealed (its letter is being shown on the redrawn board).
@@ -88,9 +92,5 @@ def get_difficulty():
 # If the two selected fields are not a match (do not have the same letters), the user is asked to press Enter. After that, the fields are concealed again.
 # The game ends when all fields are revealed.
 # The game counts the number of steps taken to complete the game and displays this number after winning the game.
-def main():
-    # write your code here
-    pass
-
 if __name__ == "__main__":
    main()
